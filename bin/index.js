85f02dc924f61723758afbc40e2eaebbf71a5731
cbillen-emburse <connor.billen@emburse.com>
2023-01-10 09:50:21 -0500

    make base directory if it doesn't exist

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const { execSync } = require('node:child_process');
 const { cwd: getCwd } = require('node:process');
 
 if (process.argv.length < 4) {
-    console.log('Usage: npx custom-npm-link [package name] [filename1] [filename2]...');
+    console.log('Usage: npx link [package name] [filename1] [filename2]...');
     return;
 }
 
@@ -20,11 +20,16 @@ try {
 
 paths.forEach((path) => {
     const localPath = resolve(cwd, 'node_modules', path);
+    const baseLocalPath = resolve(localPath, '../');
     const globalPath = resolve(globalPrefix, 'node_modules', path);
 
     if (existsSync(localPath)) {
         rmSync(localPath, { recursive: true, force: true });
     } 
+    if (!existsSync(baseLocalPath)) {
+        mkdirSync(baseLocalPath, { recursive: true });
+    }
+
     symlink(
         globalPath,
         localPath,
